ec8ac9b9212a4d0b1dd9cc1fd280678723c8bca6
Roy Contreras Benitez <roy7474@gmail.com>
2023-07-05 20:59:12 -0400

    added a few if statements, for possible errors such as non-digit entry and negative numbers

--- number_guesser.py
+++ number_guesser.py
@@ -1,5 +1,22 @@
 import random
 
+#testing samples
 # r = random.randrange(-5,11) #random numbers 
-r = random.randint(-5,11) # same as above, but now includes the uppder bound 11 
-print(r)
+# r = random.randint(-5,11) # same as above, but now includes the uppder bound 11 
+
+top_of_range = input('Type a number: ')
+
+if top_of_range.isdigit(): #isdigit is used to confirm that the value entered is a digit
+    top_of_range = int(top_of_range)
+
+    if top_of_range <=0:
+        print('Please type a number larger than 0 next time.')
+        quit()
+
+else:
+    print("Please type a number next time.")
+    quit()
+
+random_number = random.randint(0,top_of_range)
+
+print(random_number)
